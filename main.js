93bcad759305f1fab0e19d2898c5ea9f4aad219b
Albin Stjerna <albin.stjerna@gmail.com>
2016-02-14 11:20:22 +0100

    Fix indentation (again).

--- main.js
+++ main.js
@@ -11,50 +11,49 @@ var mainWindow = null;
 
 // Free resources etc.
 function cleanupQuit() {
-    console.log("QUITTING!");
-    db.close();
+  console.log("QUITTING!");
+  db.close();
 };
 
 // migrate the database as appropriate
 function dbMigrate() {
-    console.log("MIGRATING DB!");
+  console.log("MIGRATING DB!");
 };
 
 var db = null;
 
 app.on('before-quit', cleanupQuit);
 
-
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform != 'darwin') {
-        app.quit();
-    }
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
 
-    db = new sqlite3.Database(':memory:',
-                              sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
-                              dbMigrate);
+  db = new sqlite3.Database(':memory:',
+                            sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+                            dbMigrate);
 
-    // and load the index.html of the app.
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-    });
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
 });
